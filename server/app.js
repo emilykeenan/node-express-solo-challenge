4b12494bcff31c8f0614e8f0e6b656c38ce34cab
emilykeenan <emilyjanekeenan@gmail.com>
2016-11-14 14:02:43 -0600

    server set up

--- server/app.js
+++ server/app.js
@@ -1,3 +1,7 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
 // initial jokes provided by the client
 var jokes = [
   {
@@ -16,3 +20,22 @@ var jokes = [
     punchLine: "If you pee on them they disappear."
   }
 ];
+
+// routing modules
+var index = require('./routes/index');
+
+// static files
+app.use('/', index);
+
+// setting port to listen on
+// set port to listen on
+app.set('port', process.env.PORT || 4000);
+
+app.listen(app.get('port'), function() {
+  console.log('The server is listening on port ' + app.get('port'));
+});
+
+// response to send jokes array to DOM
+app.get('/jokes', function(req, res) {
+  res.send(jokes);
+});
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,5 @@
 console.log('js loaded');
+
+$(document).ready(function() {
+  console.log('JQ loaded');
+});
